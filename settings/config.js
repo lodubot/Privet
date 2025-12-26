fd2d986ab2cdb503a98d9c2b010b6dc74ea0c4aa
lodubot <jaihacku@gmail.com>
2025-12-26 07:32:19 +0530

    Create config.js

---
+++ settings/config.js
@@ -0,0 +1,8 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+global.owner = ['46731299491']
+global.packname = "MOTU_PATALU_HINDU_HAI: "
+global.author = "MOTU_PATALU_HINDU_HAI"
+global.mess = {
+ owner: '*For Owner*', //MOTU_PATALU_HINDU_HAI
+ premium: '*For Vip*'
+}
